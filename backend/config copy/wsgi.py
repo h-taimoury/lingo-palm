cb25f460d7de8194275326ee80ddfe4228eac165
h-taimoury <hosseintaimoury@gmail.com>
2026-07-16 11:00:36 +0330

    Add users app and fix manage.py

--- backend/config copy/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""WSGI config for lingo_palm."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
-application = get_wsgi_application()
